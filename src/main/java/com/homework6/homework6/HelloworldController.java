1912d0621cebeb3741dcb91c8da357671d05fff5
dyson <dyson0326@docomo.ne.jp>
2023-09-21 17:31:06 +0900

    Hello Worldから日時データを表示するように変更

--- src/main/java/com/homework6/homework6/HelloworldController.java
+++ src/main/java/com/homework6/homework6/HelloworldController.java
@@ -3,10 +3,23 @@ package com.homework6.homework6;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+
 @RestController
 public class HelloworldController {
-    @GetMapping("/hello")
-    public String hello(){
-        return "Hello World";
+    @GetMapping("/local")
+
+    public String local(){
+        return LocalDateTime.now().getMonthValue()+"月"
+                +LocalDateTime.now().getDayOfMonth()+"日";
+    }
+
+    @GetMapping("/zone")
+
+    public String zone(){
+        return ZonedDateTime.now().getHour()+"時"
+                +ZonedDateTime.now().getMinute()+"分"
+                +"　タイムゾーン"+ZonedDateTime.now().getZone();
     }
 }
