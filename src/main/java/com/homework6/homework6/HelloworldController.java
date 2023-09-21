42885074f9b2b36a19152561f7786490bb1901a0
dyson <dyson0326@docomo.ne.jp>
2023-09-21 20:32:28 +0900

    JSON形式で表示できるように変更

--- src/main/java/com/homework6/homework6/HelloworldController.java
+++ src/main/java/com/homework6/homework6/HelloworldController.java
@@ -5,21 +5,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
+import java.util.Map;
 
 @RestController
 public class HelloworldController {
     @GetMapping("/local")
-
-    public String local(){
-        return LocalDateTime.now().getMonthValue()+"月"
-                +LocalDateTime.now().getDayOfMonth()+"日";
+    public Map<String,String> local(){
+        return Map.of("timestamp",LocalDateTime.now().getMonthValue()+"月"
+                +LocalDateTime.now().getDayOfMonth()+"日");
     }
 
     @GetMapping("/zone")
-
-    public String zone(){
-        return ZonedDateTime.now().getHour()+"時"
+    public Map<String,String> zone(){
+        return Map.of("timestamp",ZonedDateTime.now().getHour()+"時"
                 +ZonedDateTime.now().getMinute()+"分"
-                +"　タイムゾーン"+ZonedDateTime.now().getZone();
+                +"　タイムゾーン"+ZonedDateTime.now().getZone());
     }
 }
